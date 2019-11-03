766c1e0f6eb76867a0ff138f3c8beb212dc76c5f
1832923 <55817450+1832923@users.noreply.github.com>
2019-11-03 09:43:10 -0600

    Add files via upload

---
+++ YT-DL_Script.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+##################################################
+##				 youtube-dl SCRIPT				##
+##################################################
+#	NOTE# These commands are set to run 3x in a row in order to maximize the chance
+#		  of downloaded all of the videos in each category before moving on to the next#		  if a lot of channels are being downloaded, you may want to decrease this.
+#	Update
+youtube-dl --update
+#	Channel Download
+#Playlists
+youtube-dl --config-location "zSettings/Config/Channel_Args-Playlist.conf" -a "zSettings/Channel_List_Playlist.txt"
+youtube-dl --config-location "zSettings/Config/Channel_Args-Playlist.conf" -a "zSettings/Channel_List_Playlist.txt"
+youtube-dl --config-location "zSettings/Config/Channel_Args-Playlist.conf" -a "zSettings/Channel_List_Playlist.txt"
+#No Playlists
+youtube-dl --config-location "zSettings/Config/Channel_Args-NoPlaylist.conf" -a "zSettings/Channel_List_NoPlaylist.txt"
+youtube-dl --config-location "zSettings/Config/Channel_Args-NoPlaylist.conf" -a "zSettings/Channel_List_NoPlaylist.txt"
+youtube-dl --config-location "zSettings/Config/Channel_Args-NoPlaylist.conf" -a "zSettings/Channel_List_NoPlaylist.txt"
+#	Liked Videos
+youtube-dl --config-location "zSettings/Config/LikedVideos_Args-Playlist.conf" -a "zSettings/Liked_Videos_Playlist.txt"
+youtube-dl --config-location "zSettings/Config/LikedVideos_Args-Playlist.conf" -a "zSettings/Liked_Videos_Playlist.txt"
+youtube-dl --config-location "zSettings/Config/LikedVideos_Args-Playlist.conf" -a "zSettings/Liked_Videos_Playlist.txt"
+#	Max Quality
+youtube-dl --config-location "zSettings/Config/MaxQual_Args-Playlist.conf" -a "zSettings/Max_Quality_Playlist.txt"
+youtube-dl --config-location "zSettings/Config/MaxQual_Args-Playlist.conf" -a "zSettings/Max_Quality_Playlist.txt"
+youtube-dl --config-location "zSettings/Config/MaxQual_Args-Playlist.conf" -a "zSettings/Max_Quality_Playlist.txt"
